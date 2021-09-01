8c3e320cf67d731a80d401581860ca036fd43090
CostaBrosky <codrutz.brosky@gmail.com>
2021-09-01 10:08:01 +0200

    Couchbase new branch

--- src/main/java/it/coachbase/CoachbaseApplication.java
+++ src/main/java/it/coachbase/CoachbaseApplication.java
@@ -7,5 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class CoachbaseApplication {
     public static void main(String[] args) {
         SpringApplication.run(CoachbaseApplication.class, args);
+
     }
 }
